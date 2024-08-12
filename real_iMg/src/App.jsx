8ef0413031a273d43b67ec65db10a916971c0fe6
rishab_7701 <agarwalrishab50@gmail.com>
2024-08-13 01:11:39 +0530

    My first commit

---
+++ real_iMg/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>An ai generated web application for Realistic Images with user defined prompts!!</h1>
+    </>
+  )
+}
+
+export default App
