6abae7c3935d2a0509964c29ba549cf1c886f2a8
Nikhil Patel <nikhilid2205@gmail.com>
2024-08-14 14:43:12 +0530

    UI adjustments.

--- real_iMg/src/Components/Hero/Hero.jsx
+++ real_iMg/src/Components/Hero/Hero.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import './Hero.css'
-import logo from '../../assets/real_Ai logo.svg'
+import React from "react";
+import "./Hero.css";
+import logo from "../../assets/real_Ai logo.svg";
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className="hero">
       <img src={logo} alt="" />
-      <h1> <span>I'm Intelligent Image</span> Generator, Bringing Imagination to Life</h1>
-      <p>A Web-application for creating Realistic Images by entering prompts as user input.</p>
+      <h1>
+        {" "}
+        <span>An Intelligent Image</span> Generator, Bringing Imagination to
+        Life
+      </h1>
+      <p>
+        A Web-application for creating Realistic Images by entering prompts as
+        user input.
+      </p>
       <div className="hero-action">
         <div className="hero-input_Prompt">Enter Prompt</div>
         <div className="hero-generate_img">Generate Image</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
